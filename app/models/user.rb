68e52c16f4c367d291fa579a5f50dc87a99e743a
Nkmryy <yayoi.nakamura.8669@gmail.com>
2020-11-26 18:22:26 +0900

    Userモデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, presence: true,
-            format: {with: /\A[\w]+\z/, message: '英字と数字の両方を含めて設定してください' }
+            format: {with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
   validates :nickname, presence: true
   validates :birth_date, presence: true
   
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' } do
     validates :first_name
     validates :last_name
   end
   
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナ文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
