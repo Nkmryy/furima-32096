736a2990ead77a8ca1481ae2a4207acbe1e94815
Nkmryy <yayoi.nakamura.8669@gmail.com>
2020-12-01 12:49:35 +0900

    修正１

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name           {Faker::Name.name}
     description    {Faker::Lorem.sentence}
-    category_id    {"2"}
-    status_id      {"2"}
-    freight_id     {"2"}
-    ship_state_id  {"2"}
-    ship_day_id    {"2"}
-    price          {"2200"}
+    category_id    {2}
+    status_id      {2}
+    freight_id     {2}
+    ship_state_id  {2}
+    ship_day_id    {2}
+    price          {2200}
 
     association       :user
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe Item, type: :model do
           expect(@item).to be_valid 
         end
         it "priceが300以上であれば登録できる" do
-          @item.price = "300"
+          @item.price = 300
           expect(@item).to be_valid 
         end
         it "priceが9999999以下であれば登録できる" do
-          @item.price = "9999999"
+          @item.price = 9999999
           expect(@item).to be_valid 
         end
       end
@@ -43,12 +43,12 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include "Price is not a number"
         end
         it "priceが299以下では出品できない" do
-          @item.price = "299"
+          @item.price = 299
           @item.valid?
           expect(@item.errors.full_messages).to include "Price must be greater than 299"
         end
         it "priceが10000000以上では出品できない" do
-          @item.price = "10000000"
+          @item.price = 10000000
           @item.valid?
           expect(@item.errors.full_messages).to include "Price must be less than 10000000"
         end
@@ -63,7 +63,7 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include "Category can't be blank"
         end
         it "category_idが１の場合は出品できない" do
-          @item.category_id = "1"
+          @item.category_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include "Category must be other than 1"
         end
@@ -73,7 +73,7 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include "Status can't be blank"
         end
         it "status_idが１の場合は出品できない" do
-          @item.status_id = "1"
+          @item.status_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include "Status must be other than 1"
         end
@@ -83,7 +83,7 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include "Freight can't be blank"
         end
         it "freight_idが１の場合は出品できない" do
-          @item.freight_id = "1"
+          @item.freight_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include "Freight must be other than 1"
         end
@@ -93,7 +93,7 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include "Ship state can't be blank"
         end
         it "ship_state_idが１の場合は出品できない" do
-          @item.ship_state_id = "1"
+          @item.ship_state_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include "Ship state must be other than 1"
         end
@@ -103,7 +103,7 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include "Ship day can't be blank"
         end
         it "ship_day_idが１の場合は出品できない" do
-          @item.ship_day_id = "1"
+          @item.ship_day_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include "Ship day must be other than 1"
         end
