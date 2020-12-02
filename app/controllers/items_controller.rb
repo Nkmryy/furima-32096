871a4ebd4e6797c038ac6ec0fcecb04a761b61bf
Nkmryy <yayoi.nakamura.8669@gmail.com>
2020-12-02 19:55:59 +0900

    編集機能への画面遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,6 +30,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
   def message_params
     params.require(:item).permit(:image, :name, :description, :price, :category_id, :status_id, :freight_id, :ship_state_id, :ship_day_id).merge(user_id: current_user.id)
