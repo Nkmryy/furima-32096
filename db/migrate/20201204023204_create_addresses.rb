1f71787fe99f10f47ed8a1ba4005e341bcb9c9db
Nkmryy <yayoi.nakamura.8669@gmail.com>
2020-12-04 12:31:51 +0900

    オーダーテーブル、アドレステーブルの作成

---
+++ db/migrate/20201204023204_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :postal_code, null: false
+      t.integer :states_id,  null: false
+      t.string :address,     null: false
+      t.string :street,      null: false
+      t.string :buillding
+      t.string :phone,       null: false
+      t.references :order,    foreign_key: true
+      t.timestamps
+    end
+  end
+end
