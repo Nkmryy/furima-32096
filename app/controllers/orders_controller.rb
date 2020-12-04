2df2c902e73c7e15e786c25911a2bd3b1d2cd08b
Nkmryy <yayoi.nakamura.8669@gmail.com>
2020-12-04 19:35:57 +0900

    クレジットカード決済用のJavaScriptの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,24 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.new
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
+    binding.pry
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to action: :index
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postl_code, :state_id, :address, :street, :phone, :order_id).marge(token: params[:token])
   end
 
 end
