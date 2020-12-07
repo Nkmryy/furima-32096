188b5c6719b588789c965bc64e4c8aa81730636c
Nkmryy <yayoi.nakamura.8669@gmail.com>
2020-12-07 15:28:20 +0900

    コメントアウトの解除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,7 +20,7 @@ class OrdersController < ApplicationController
   def create
       @order_address = OrderAddress.new(order_address_params)
       if @order_address.valid?
-        #pay_item
+        pay_item
         @order_address.save
         redirect_to root_path
       else
