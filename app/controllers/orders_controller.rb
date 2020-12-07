a812385e8f23ba351578e1d742190df829e72469
Nkmryy <yayoi.nakamura.8669@gmail.com>
2020-12-07 13:54:28 +0900

    商品が購入済みの場合購入画面へ遷移できないように変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,31 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+
   def index
-    @item = Item.find(params[:item_id])
-    @order_address = OrderAddress.new
+    if @order == nil
+      if user_signed_in?
+        if @item.user_id != current_user.id
+          @order_address = OrderAddress.new
+        else
+          redirect_to root_path
+        end
+      else
+        redirect_to new_user_session_path
+      end
+    else
+      redirect_to root_path
+    end
   end
 
   def create
-    @item = Item.find(params[:item_id])
-    @order_address = OrderAddress.new(order_address_params)
-    if @order_address.valid?
-      pay_item
-      @order_address.save
-      redirect_to root_path
-    else
-      render :index
-    end
+      @order_address = OrderAddress.new(order_address_params)
+      if @order_address.valid?
+        #pay_item
+        @order_address.save
+        redirect_to root_path
+      else
+        render :index
+      end
   end
 
   private
@@ -31,4 +43,8 @@ class OrdersController < ApplicationController
     )
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+    @order = Order.find_by(item_id: @item.id)
+  end
 end
